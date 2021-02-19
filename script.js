7df3215a35b0afe2a2ea93417d7416ca1d49ea4a
yabakoni <79301017+yabakoni@users.noreply.github.com>
2021-02-19 08:54:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+
 var counter=0;
 function jump(){
     if(character.classList == "animate"){return}
